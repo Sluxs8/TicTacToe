bf05174581569d1a98b346a3fec7766a843b8040
Sluxs8 <rabironald22@gmail.com>
2023-01-07 09:37:31 +0100

    Nyerés fixálva

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace TicTacToe
         public static StringBuilder epito456 = new StringBuilder(" 4 | 5 | 6 ");
         public static StringBuilder epito789 = new StringBuilder(" 7 | 8 | 9 ");
         public static List<int> FelulirtSzamok = new List<int>();
+        public static List<int> xTarolt = new List<int>();
+        public static List<int> oTarolt = new List<int>();
+        
         static void Main(string[] args)
         {
             TicTacToe();
@@ -23,6 +26,10 @@ namespace TicTacToe
             //atirja a mezot
             //kovetkezo jatekos valaszt mezot
             //megnezni h valaki nyert-e, ha tele a tabla dontetlen
+
+            Console.WriteLine("Írd be egy mező számát");
+
+            //eltárolni melyik játékos hova rakott, mind a 8 lehetőséget megadni (switch) (azok a számok mind x vagy o)
 
 
                 Console.WriteLine(" 1 | 2 | 3 ");
@@ -46,7 +53,7 @@ namespace TicTacToe
             XjatekosKore();
 
 
-
+            
 
         }
         static void Cserelo(int beolvasas, char cserekarakter)
@@ -105,6 +112,7 @@ namespace TicTacToe
             { 
                 Cserelo(beolvasas, 'x');
                 Kiir();
+                Nyeres(beolvasas, 'x');
             }
 
         }
@@ -118,6 +126,8 @@ namespace TicTacToe
             {
                 Cserelo(beolvasas, 'o');
                 Kiir();
+                Nyeres(beolvasas, 'o');
+
             }
             
             
@@ -159,5 +169,152 @@ namespace TicTacToe
 
             
         }
+
+        static void Nyeres(int beolvasas, char jatekos)
+        {
+            bool jatekosnyert = false;
+            List<int> jatekosTarolt;
+            if (jatekos == 'x')
+            {
+                jatekosTarolt = xTarolt;
+            }
+            else
+            {
+                jatekosTarolt = oTarolt;
+            }
+
+            switch (beolvasas)
+            {
+                //123 = x akkor x jatekos nyer xJatekos.Add(x)
+                case 1:
+                    if (jatekosTarolt.Contains(2) && jatekosTarolt.Contains(3))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(4) && jatekosTarolt.Contains(7))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(5) && jatekosTarolt.Contains(9))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 2:
+                    if (jatekosTarolt.Contains(1) && jatekosTarolt.Contains(3))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(5) && jatekosTarolt.Contains(8))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 3:
+                    if (jatekosTarolt.Contains(1) && jatekosTarolt.Contains(2))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(5) && jatekosTarolt.Contains(7))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(6) && jatekosTarolt.Contains(9))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 4:
+                    if (jatekosTarolt.Contains(5) && jatekosTarolt.Contains(6))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(1) && jatekosTarolt.Contains(7))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 5:
+                    if (jatekosTarolt.Contains(1) && jatekosTarolt.Contains(9))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(7) && jatekosTarolt.Contains(3))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(4) && jatekosTarolt.Contains(6))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(2) && jatekosTarolt.Contains(8))
+                    {
+                        jatekosnyert = true;
+                    }
+                      break;
+
+                case 6:
+                    if (jatekosTarolt.Contains(4) && jatekosTarolt.Contains(5))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(9) && jatekosTarolt.Contains(3))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 7:
+                    if (jatekosTarolt.Contains(1) && jatekosTarolt.Contains(4))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(8) && jatekosTarolt.Contains(9))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(5) && jatekosTarolt.Contains(3))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 8:
+                    if (jatekosTarolt.Contains(2) && jatekosTarolt.Contains(5))
+                    {
+                        jatekosnyert = true;
+                    }
+                    else if (jatekosTarolt.Contains(7) && jatekosTarolt.Contains(9))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+
+                case 9:
+                    if (jatekosTarolt.Contains(1) && jatekosTarolt.Contains(5))
+                    {
+                        jatekosnyert = true;
+                    }
+                    if (jatekosTarolt.Contains(6) && jatekosTarolt.Contains(3))
+                    {
+                        jatekosnyert = true;
+                    }
+                    if (jatekosTarolt.Contains(7) && jatekosTarolt.Contains(8))
+                    {
+                        jatekosnyert = true;
+                    }
+                    break;
+            }
+            jatekosTarolt.Add(beolvasas);
+            if (jatekosnyert)
+            {
+                Console.WriteLine(jatekos + " játékos nyert");
+            }
+            
+        } 
     }
 }
